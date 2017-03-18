5aefdf1c2bfed106222ce548f4d398c9505958e9
Liseth Rendon <lisjazrendon@gmail.com>
2017-03-18 11:16:59 -0500

    Corregido el php dos

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
 	<title>Prueba App Empresariales </title>
 </head>
+
 <body>
-	<?php echo '<p>Hola Apps Empresariales</p>; ?>
+	<?php echo '<p>Hola Apps Empresariales</p>'; ?>
 </body>
 
 </html>
